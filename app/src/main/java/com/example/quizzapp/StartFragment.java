06b35814b4b00612cc38d84d9540d8df67a4c41e
YaroslavSulyma <45691318+YaroslavSulyma@users.noreply.github.com>
2020-03-02 23:15:02 +0200

    created start fragment and firebase auth

--- app/src/main/java/com/example/quizzapp/StartFragment.java
+++ app/src/main/java/com/example/quizzapp/StartFragment.java
@@ -2,17 +2,36 @@ package com.example.quizzapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class StartFragment extends Fragment {
+
+    private ProgressBar startProgress;
+    private TextView startFeedbackText;
+    private FirebaseAuth firebaseAuth;
+    private static final String START_TAG = "START LOG";
+    private NavController navController;
 
     public StartFragment() {
         // Required empty public constructor
@@ -24,5 +43,47 @@ public class StartFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_start, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        navController = Navigation.findNavController(view);
+
+        startProgress = view.findViewById(R.id.fragment_start_progress);
+        startFeedbackText = view.findViewById(R.id.fragment_start_feedback);
+
+        startFeedbackText.setText(R.string.checkingUserAcoount);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+        if(currentUser == null){
+
+            startFeedbackText.setText(R.string.creatingAccount);
+
+            //Create  anew account
+            firebaseAuth.signInAnonymously().addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        startFeedbackText.setText(R.string.accountCreated);
+                        navController.navigate(R.id.action_startFragment_to_listFragment);
+                    }else{
+                        Log.d(START_TAG, "Start Log:" + task.getException());
+                    }
+                }
+            });
+        }else{
+            //Navigate to the homepage
+            startFeedbackText.setText(R.string.loggedIn);
+            navController.navigate(R.id.action_startFragment_to_listFragment);
+        }
     }
 }
