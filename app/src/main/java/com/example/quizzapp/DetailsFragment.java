d15afc7fdba51c77e2267608088cab44d0904c01
YaroslavSulyma <45691318+YaroslavSulyma@users.noreply.github.com>
2020-04-11 18:00:24 +0300

    details fragment finished

--- app/src/main/java/com/example/quizzapp/DetailsFragment.java
+++ app/src/main/java/com/example/quizzapp/DetailsFragment.java
@@ -5,17 +5,43 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+import java.util.Objects;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DetailsFragment extends Fragment {
+public class DetailsFragment extends Fragment implements View.OnClickListener {
+    private NavController navController;
+    private QuizListViewModel quizListModel;
+    private int position;
+
+    private ImageView detailsImage;
+    private TextView detailsTitle;
+    private TextView detailsDesc;
+    private TextView detailsDiff;
+    private TextView detailsQuestions;
+    private Button detailsStartButton;
+
 
     public DetailsFragment() {
         // Required empty public constructor
@@ -33,7 +59,45 @@ public class DetailsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        int position = DetailsFragmentArgs.fromBundle(getArguments()).getPosition();
-        Log.d("APP_LOG", "Position: " + position);
+        navController = Navigation.findNavController(view);
+
+        position = DetailsFragmentArgs.fromBundle(getArguments()).getPosition();
+
+        //Initialize UI Elements
+        detailsImage = view.findViewById(R.id.fragment_details_image);
+        detailsTitle = view.findViewById(R.id.fragment_details_title);
+        detailsDesc = view.findViewById(R.id.fragment_details_description);
+        detailsDiff = view.findViewById(R.id.fragment_details_difficulty_text);
+        detailsQuestions = view.findViewById(R.id.fragment_details_question_text);
+        detailsStartButton = view.findViewById(R.id.fragment_details_start_button);
+        detailsStartButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        quizListModel = new ViewModelProvider(Objects.requireNonNull(getActivity())).get(QuizListViewModel.class);
+        quizListModel.getQuizListModelData().observe(getViewLifecycleOwner(), new Observer<List<QuizListModel>>() {
+            @Override
+            public void onChanged(List<QuizListModel> quizListModels) {
+                Glide.with(getContext()).load(quizListModels.get(position).getImage()).centerCrop().placeholder(R.drawable.placeholder_image).into(detailsImage);
+                detailsTitle.setText(quizListModels.get(position).getName());
+                detailsDesc.setText(quizListModels.get(position).getDescription());
+                detailsDiff.setText(quizListModels.get(position).getLevel());
+                detailsQuestions.setText(quizListModels.get(position).getQuestions() + "");
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.fragment_details_start_button:
+                DetailsFragmentDirections.ActionDetailsFragmentToQuizFragment action = DetailsFragmentDirections.actionDetailsFragmentToQuizFragment();
+                action.setPosition(position);
+                navController.navigate(action);
+                break;
+        }
     }
 }
